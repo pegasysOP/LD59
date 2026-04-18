6ada39db05a04c67647e339873cb2aba5628a087
LGNDLeviathan <roitchie@gmail.com>
2026-04-18 16:36:11 +0100

    Add script to open door

---
+++ Assets/Scripts/Door/Door.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    private bool isClosed = true;
+
+    [SerializeField]
+    private GameObject doorPanel;
+
+    private const float OpenOffset = -1.45f;
+    private const float OpenDuration = 0.5f;
+
+    public void Interact()
+    {
+        Debug.Log("Interacting with door");
+        if (isClosed && doorPanel != null)
+        {
+            StartCoroutine(MoveDoor(OpenOffset, OpenDuration));
+        }
+    }
+
+    public bool IsInteractable()
+    {
+        return isClosed;
+    }
+
+    private IEnumerator MoveDoor(float deltaZ, float duration)
+    {
+        isClosed = false;
+
+        Transform t = doorPanel.transform;
+        Vector3 start = t.localPosition;
+        Vector3 target = start + new Vector3(0f, 0f, deltaZ);
+
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float frac = Mathf.Clamp01(elapsed / duration);
+            t.localPosition = Vector3.Lerp(start, target, frac);
+            yield return null;
+        }
+
+        t.localPosition = target;
+    }
+}
