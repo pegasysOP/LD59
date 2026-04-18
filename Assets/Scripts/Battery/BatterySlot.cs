48244f54ab77f06784a7e1b7250d3ca9b84ea954
LGNDLeviathan <roitchie@gmail.com>
2026-04-18 12:46:33 +0100

    Create battery slot and place it in scene

---
+++ Assets/Scripts/Battery/BatterySlot.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class BatterySlot : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private Battery.BatteryColour slotColour;
+
+    public void Interact()
+    {
+        //TODO: Add a trigger and if within the trigger then automatically call this interact 
+
+        //TODO: Maybe play a sound when trying to place nothing in the slot?
+        //Set the light to green when the correct battery is placed in the slot?
+        return;
+    }
+
+    public bool IsInteractable()
+    {
+        return true;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Battery battery = other.GetComponent<Battery>();
+        if (battery != null)
+        {
+            Debug.Log("Battery placed in a slot!");
+            Interact();
+
+            if(battery.colour == slotColour)
+            {
+                //TODO: Set the batterys position to be in the middle of the slot and make it so it can't be picked up again.
+                Debug.Log("Battery placed in correct slot!");
+                battery.isInCorrectSlot = true;
+
+                
+            }
+
+        }
+    }
+}
