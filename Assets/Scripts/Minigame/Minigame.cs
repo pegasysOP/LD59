4fec74e6a5c0c33a75d1e2d196689c74356b13ea
pegasys <sconnolly7@outlook.com>
2026-04-18 12:31:22 +0100

    add placeholder ui

---
+++ Assets/Scripts/Minigame/Minigame.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Minigame : MonoBehaviour
+{
+    public Image alienImage;
+
+    public TextMeshProUGUI alienText;
+    public TextMeshProUGUI playerText;
+}
