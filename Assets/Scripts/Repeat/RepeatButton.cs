be1f50738fbdd0ada8c43413aca6ff785029bb36
LGNDLeviathan <roitchie@gmail.com>
2026-04-18 19:54:35 +0100

    Flash button colours

--- Assets/Scripts/Repeat/RepeatButton.cs
+++ Assets/Scripts/Repeat/RepeatButton.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RepeatButton : MonoBehaviour, IInteractable
 {
@@ -15,7 +17,43 @@ public class RepeatButton : MonoBehaviour, IInteractable
         Debug.Log($"Button {colour} was pressed");
 
         OnPressed?.Invoke(colour);
-        //TODO: Something?
+        
+        Flash(0.2f);
+    }
+
+    private bool flashing = false;
+
+    public void Flash(float duration)
+    {
+        if (flashing) return;
+        StartCoroutine(FlashRoutine(duration));
+    }
+
+    private IEnumerator FlashRoutine(float duration)
+    {
+        flashing = true;
+
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+
+        Color original = Color.white;
+        bool hasOriginal = false;
+
+        if (meshRenderer != null)
+        {
+            original = meshRenderer.material.color;
+            hasOriginal = true;
+            Color bright = new Color(Mathf.Min(original.r * 1.8f, 1f), Mathf.Min(original.g * 1.8f, 1f), Mathf.Min(original.b * 1.8f, 1f), original.a);
+            meshRenderer.material.color = bright;
+        }
+
+        yield return new WaitForSeconds(duration);
+
+        if (hasOriginal)
+        {
+            if (meshRenderer != null) meshRenderer.material.color = original;
+        }
+
+        flashing = false;
     }
 
     public bool IsInteractable()
