5369bc9032508433f20fed621ea4e91aacd08da3
LGNDLeviathan <roitchie@gmail.com>
2026-04-18 10:39:58 +0100

    Add TODO comments

---
+++ Assets/Scripts/Player/PlayerInteractions.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    //TODO: Implement raycasting and calling objects interact methods, such as picking up batteries, interacting with doors, etc.
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
