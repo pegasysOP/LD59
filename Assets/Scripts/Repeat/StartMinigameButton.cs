891cd5aca862a5275a46daa98bb8b5672380208e
LGNDLeviathan <roitchie@gmail.com>
2026-04-20 10:34:23 +0100

    Add idle flash to repeat minigame button

--- Assets/Scripts/Repeat/StartMinigameButton.cs
+++ Assets/Scripts/Repeat/StartMinigameButton.cs
@@ -11,9 +11,21 @@ public class StartMinigameButton : BaseButton, IInteractable
     [SerializeField]
     private float flashTime = 0.2f;
 
+    private Coroutine idleFlashRoutine;
+
+    public void Start()
+    {
+        if (IsInteractable())
+            idleFlashRoutine = StartCoroutine(IdleFlashLoop());
+    }
+
     public void Interact()
     {
         hasStarted = true;
+
+        if (idleFlashRoutine != null)
+            StopCoroutine(idleFlashRoutine);
+
         minigame?.SoundConfig?.PlayStartMinigameAt(transform.position);
         Flash(flashTime);
         minigame.StartMinigame();
@@ -22,5 +34,14 @@ public class StartMinigameButton : BaseButton, IInteractable
     public bool IsInteractable()
     {
         return !hasStarted;
+    }
+
+    private IEnumerator IdleFlashLoop()
+    {
+        while (!hasStarted)
+        {
+            Flash(flashTime);
+            yield return new WaitForSeconds(Random.Range(1.7f, 2.2f));
+        }
     }
 }
