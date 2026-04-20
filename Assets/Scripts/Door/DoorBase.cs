7ca4fbae78a248461986626d39ef8ffa345df3fb
LGNDLeviathan <roitchie@gmail.com>
2026-04-20 14:58:01 +0100

    Create base class for Door

---
+++ Assets/Scripts/Door/DoorBase.cs
@@ -0,0 +1,42 @@
+using TreeEditor;
+using UnityEngine;
+
+public class DoorBase : MonoBehaviour, IInteractable
+{
+    protected bool isClosed = true;
+    protected float initialTimer = 0f;
+
+    [SerializeField]
+    protected MeshRenderer meshRenderer;
+
+    [SerializeField]
+    protected Material greenMaterial;
+    [SerializeField]
+    protected Material redMaterial;
+
+    public virtual void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public virtual bool IsInteractable()
+    {
+        if ((isClosed && initialTimer <= 0) == true)
+        {
+            meshRenderer.material = redMaterial;
+        }
+        return isClosed && initialTimer <= 0;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
