bdfaa6b5a730ff55c0e5f0f3cbd327124f840d05
LGNDLeviathan <roitchie@gmail.com>
2026-04-25 09:20:41 +0100

    Add MoveDoor method to base class

--- Assets/Scripts/Door/DoorBase.cs
+++ Assets/Scripts/Door/DoorBase.cs
@@ -1,7 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoorBase : MonoBehaviour, IInteractable
 {
+    [SerializeField]
+    protected GameObject doorPanel;
+
     protected bool isClosed = true;
     protected float initialTimer = 0f;
 
@@ -27,15 +31,32 @@ public class DoorBase : MonoBehaviour, IInteractable
         return isClosed && initialTimer <= 0;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected virtual void OnDoorOpened()
     {
-        
+        StateTracker.Instance?.NotifyStartingDoorOpened();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected IEnumerator MoveDoor(float deltaZ, float duration)
     {
-        
+        isClosed = false;
+
+        Transform t = doorPanel.transform;
+        Vector3 start = t.localPosition;
+        Vector3 target = start + new Vector3(0f, 0f, deltaZ);
+
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float frac = Mathf.Clamp01(elapsed / duration);
+            t.localPosition = Vector3.Lerp(start, target, frac);
+            yield return null;
+        }
+
+        t.localPosition = target;
+
+        meshRenderer.material = redMaterial;
+
+        OnDoorOpened();
     }
 }
